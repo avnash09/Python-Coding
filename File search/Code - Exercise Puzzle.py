adcb116f9956e2f2cf8560792174aab712fc3db0
Avinash Anand <avinash.rkl09@gmail.com>
2025-03-29 20:55:01 +0530

    Initial commit; Added a file search code to find a phone number from a bunch of files in various directories

---
+++ File search/Code - Exercise Puzzle.py
@@ -0,0 +1,44 @@
+import shutil, os, re, pdb
+filename = 'unzip_me_for_instructions.zip'
+dir_to_unzip = os.getcwd()
+
+shutil.unpack_archive(filename, '', format='zip')
+
+cwd = dir_to_unzip #Current working Directory
+with open(cwd + '\\extracted_content\\Instructions.txt', 'r') as file:
+    print('Instructions below:\n'+file.read()+'\n')
+
+#set the pattern for the phone number
+search_pattern = r'\d{3}-\d{3}-\d{4}'
+
+#define a function to seach the pattern within the file
+def pattern_search(file, pattern = r'\d{3}-\d{3}-\d{4}'):
+    f = open(file, 'r')
+    text = f.read()
+
+    if re.search(pattern, text):
+        #returning a Tuple
+        return (re.search(pattern, text), file)
+
+#Set working directory
+work_dir = cwd + '\\test_folder'
+#os.chdir(work_dir)
+
+result = []
+
+#find all the files present with os.walk()
+for folder, subfolders, files in os.walk(work_dir):
+    #print('Currently looking at the folder:\n'+folder)
+    
+    for file in files:
+        full_path = folder+'\\'+file
+
+        result.append(pattern_search(full_path, search_pattern))
+
+for r in result:
+    if r:
+        match, file = r #Tuple unpacking
+        print('Phone number: '+match.group())
+        print(f'Full Path: \n\t{file}')
+        print(f'\nFilepath: {'\\'.join(re.findall(r'[^\\]+', file)[:-1])}')
+        print(f'\nFilename: {re.search(r'[^\\]+$', file).group()}')
