6fe4465658a15547cbf131149f6dd0eb8c44b1b5
Avinash Anand <avinash.rkl09@gmail.com>
2025-04-14 23:34:13 +0530

    Initial commit; Unfinished code

---
+++ Object Oriented Programming/turtle_graphics.py
@@ -0,0 +1,18 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# myscreen = Screen()
+
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.color('coral')
+
+# def square(turtle):
+#     for _ in range(4):
+#         turtle.fd(100)
+#         turtle.tilt(90)
+
+# timmy.tilt(90)
+# timmy.fd(90)
+# #square(timmy)
+# myscreen.exitonclick()
