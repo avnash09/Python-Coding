892944d8d9c44b22fcfe86ef50f46d9733dbfbbd
Avinash Macbook Air <avinash.rkl09@gmail.com>
2025-04-21 23:17:52 +0530

    Rectagular shape; draft formulation ready

--- Turtle/rough_script.py
+++ Turtle/rough_script.py
@@ -12,8 +12,24 @@ s = Screen()
 
 t.speed("fastest")
 s.colormode(255)
-no_of_dots = input("Enter the number of dots: ")
-print(s.screensize())
-print(no_of_dots)
+dots = 100 #input("Enter the number of dots: ")
+rows = 10
+dot_size = 10
+step_size = dot_size * 2
+dots_each_row = dots / rows
+home_xcor = -1 * (500 / dots) * (dots_each_row + step_size)
+t.setpos(home_xcor, 0)
+
+for y in range(1, dots + 1):
+    t.dot(dot_size, 'black')
+    t.forward(step_size)
+
+    if y % 10 == 0:
+        t.setheading(90)
+        t.forward(step_size)
+        t.setheading(0)
+        t.backward((step_size) * dots_each_row)
+
+
 
 s.exitonclick()
