7565ee2a8845bc309464a52dd6e3ec77f8a42e2a
Avinash Macbook Air <avinash.rkl09@gmail.com>
2025-04-23 15:09:17 +0530

    added Triangle function to create triangle shape Hirst Painting; segregated the required variables as per the shape

--- Turtle/rough_script.py
+++ Turtle/rough_script.py
@@ -7,22 +7,6 @@ if os.name == 'nt':
 else:
     os.system('clear')
 
-t = Turtle()
-s = Screen()
-
-t.speed("fastest")
-s.colormode(255)
-t.penup()
-dots = 100 #input("Enter the number of dots: ")
-rows = 10
-dot_size = 20
-step_size = dot_size * 1.5
-dots_each_row = dots / rows
-start_xcor = -1 * ((step_size) * dots_each_row/2) + (step_size/2)
-start_ycor = -1 * ((step_size) * dots_each_row/2) + (step_size/2)
-#print(start_xcor, ',', start_ycor)
-t.setpos(start_xcor, start_ycor)
-
 def get_rgb():
 
     r = random.randint(0,255)
@@ -31,17 +15,64 @@ def get_rgb():
 
     return (r, g, b)
 
-for y in range(1, dots + 1):
-    t.dot(dot_size, get_rgb())
-    t.forward(step_size)
+t = Turtle()
+s = Screen()
 
-    if y % 10 == 0:
-        #print(y, t.pos())
+t.speed("fastest")
+s.colormode(255)
+t.penup()
+#t.hideturtle()
+#print(start_xcor, ',', start_ycor)
+
+def quadrilateral():
+
+    dots = 100
+    rows = 5
+    columns = int(dots / rows)
+    dot_size = 20
+    step_size = dot_size * 1.5
+    dots_each_row = int(dots / rows)
+    dots_each_col = int(dots / columns)
+
+    start_xcor = -1 * ((step_size) * dots_each_row/2) + (step_size/2)
+    start_ycor = -1 * ((step_size) * dots_each_col/2) + (step_size/2)
+
+    t.setpos(start_xcor, start_ycor)
+
+    for y in range(1, dots + 1):
+        t.dot(dot_size, get_rgb())
+        t.forward(step_size)
+
+        if y % dots_each_row == 0:
+            #print(y, t.pos())
+            t.setheading(90)
+            t.forward(step_size)
+            t.setheading(0)
+            t.backward(step_size * dots_each_row)
+
+def triangle():
+    
+    rows = 15
+    dot_size = 20
+    step_size = dot_size * 1.5
+
+    start_xcor = -1 * ((step_size) * rows) / 2
+    start_ycor = -1 * ((step_size) * rows) / 2
+
+    t.setpos(start_xcor, start_ycor)
+    
+    for i in range(rows+1, 0, -1):
+        
+        for _ in range(1, i):
+            t.dot(dot_size, get_rgb())
+            t.forward(step_size)
+        
         t.setheading(90)
         t.forward(step_size)
         t.setheading(0)
-        t.backward((step_size) * dots_each_row)
+        t.backward((step_size)*(i-1))
+        t.forward(step_size/2)
 
-
+quadrilateral()
 
 s.exitonclick()
