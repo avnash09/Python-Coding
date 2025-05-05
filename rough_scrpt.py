ef90f335c26fd18d286a5e19f24adf519a44d20e
Avinash Anand <avinash.rkl09@gmail.com>
2025-05-05 14:46:55 +0530

    Rough code script

---
+++ rough_scrpt.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+
+print(add(4,5))
+
+
+
+my_function = "This is a variable."
+
+def my_function():
+    return "This is a function."
+
+print(my_function) # Output: This is a variable
+
+
