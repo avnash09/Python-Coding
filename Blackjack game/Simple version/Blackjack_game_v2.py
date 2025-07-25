3f67c93a828b8bbe487ed8d1e3ff9e006c308721
Avinash Anand <avinash.rkl09@gmail.com>
2025-07-25 13:58:19 +0530

    Blackjack Simpler version: Code structure updated, removed unwanted code snippets

--- Blackjack game/Simple version/Blackjack_game_v2.py
+++ Blackjack game/Simple version/Blackjack_game_v2.py
@@ -11,13 +11,13 @@ def clear_terminal():
 def start_game():
     
     while True:
-            start = input("Do you want to play a game of Blackjack? (y/n): ")
-            if start and start[0].upper() == 'Y':
-                return True
-            elif start and start[0].upper() == 'N':
-                return False
-            else:
-                print("Please provide a valid input!")
+        start = input("Do you want to play a game of Blackjack? (y/n): ")
+        if start[0].upper() == 'Y':
+            return True
+        elif start[0].upper() == 'N':
+            return False
+        else:
+            print("Please provide a valid input!")
 
 
 clear_terminal()
@@ -40,6 +40,27 @@ def score(cards):
     
     return sum(cards)
 
+#Compare scores
+def compare_scores(player_score, dealer_score):
+    
+    clear_terminal()
+    print(blackjack_art.logo)
+
+    if player_score == 0:
+        print("You win with a Blackjack!")
+    elif dealer_score == 0:
+        print("Dealer has a Blackjack! You lose.")
+    elif player_score > 21:
+        print(f"BUST! You went over, you lose.")
+    elif dealer_score > 21:
+        print("Dealer went over, You WIN!")
+    elif dealer_score == player_score:
+        print("Draw!")
+    elif dealer_score > player_score:
+        print("Dealer wins. Sorry, You lose!")
+    else:
+        print("Congratulations! You WIN!")  
+
 while game_on:
 
     #players
@@ -50,31 +71,26 @@ while game_on:
     for _ in range(2):
         player_cards.append(deal_card())
         computer_cards.append(deal_card())
+
+    # player_cards = [4,10,7]
+    #computer_cards = [10,11]
     
     clear_terminal()
     print(blackjack_art.logo)
     print("Let's Play!")
 
-    while True:
+    while score(player_cards) != 0 and score(computer_cards) != 0 and score(player_cards) <= 21:
 
         #display initial hands
         print(f"Your cards: {player_cards}, score: {score(player_cards)}")
         print(f"Computer's first hand: {computer_cards[0]}")
-        if score(player_cards) == 0:
-            print("You win with a Blackjack!")
-            break
-        elif score(computer_cards) == 0:
-            print("Dealer wins with a Black! You lose.")
-            break
 
         user_input = input("\nDo you want to take another card? (y/n): ")
         if user_input[0].upper() == 'Y':
             player_cards.append(deal_card())
-            clear_terminal()
-            print(blackjack_art.logo)
-            if score(player_cards) > 21:
-                print(f"BUST! You went over, you lose.")
-                break
+            # clear_terminal()
+            # print(blackjack_art.logo)
+            # break
 
         elif user_input[0].upper() == 'N':
             clear_terminal()
@@ -83,21 +99,13 @@ while game_on:
             #Dealers deals till his score is greater than 17
             while score(computer_cards) < 17:
                 computer_cards.append(deal_card())
-
-            if score(computer_cards) > 21:
-                print("Dealer went over, You WIN!")
-            elif score(computer_cards) == score(player_cards):
-                print("Draw!")
-            elif score(computer_cards) < score(player_cards):
-                print("Congratulations! You WIN!")
-            else:
-                print("Dealer wins. Sorry, You lose!")
             break
 
         else:
             print("Invalid input! Try again.")
 
+    compare_scores(player_score=score(player_cards), dealer_score=score(computer_cards))
     print(f"Your cards: {player_cards}, Your Score: {score(player_cards)}")
     print(f"Computer's hand: {computer_cards}, Computer's Score: {score(computer_cards)}")
         
-    game_on = True if input("\nDo you want to play again? (y/n): ")[0].upper() == 'Y' else False
+    game_on = True if input("\nDo you want to play again? (y/n):")[0].upper() == 'Y' else False
