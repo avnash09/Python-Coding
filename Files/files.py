7d70e83ce3d30e8f59861feb09e9fa9a46a4e7a9
Avinash Anand <avinash.rkl09@gmail.com>
2025-08-11 19:16:40 +0530

    Files: Added some practice code & files

--- Files/files.py
+++ Files/files.py
@@ -9,8 +9,8 @@ os.system('cls')
 
 filepath = os.getcwd() + "\Files\\"
 
-with open(f"{filepath}my_file.txt") as file:
-    contents = file.read()
+# with open(f"{filepath}my_file.txt") as file:
+#     contents = file.read()
     # print(contents)
 
 # with open(f"{filepath}my_file.txt", mode="a") as file:
@@ -19,5 +19,19 @@ with open(f"{filepath}my_file.txt") as file:
 # with open(f"{filepath}new_file.txt", mode="w") as file:
 #     file.write(input("File contents: "))
 
-with open(f"{filepath}new_file.txt", mode="r") as file:
-    print(file.read())
+# with open(f"{filepath}new_file.txt", mode="r") as file:
+#     print(file.read())
+
+file1 = []
+with open(filepath + 'file1.txt', mode='r') as f:
+    file1 = f.readlines()
+    
+file2 = []
+with open(filepath + 'file2.txt', mode='r') as f:
+    file2 = f.readlines()
+
+import pandas as pd
+data = pd.read_csv(filepath + 'file2.txt')
+# print(data)
+
+print([int(num) for num in file1 if num in file2])
