44c129123de5330b2de50191b3fa7aa0b37f296a
Avinash Anand <avinash.rkl09@gmail.com>
2025-08-13 22:25:59 +0530

    args-kwargs: revisiting the code

--- args kwargs.py
+++ args kwargs.py
@@ -3,4 +3,27 @@ def myfunc(*args, **kwargs):
     print(kwargs)
     print(f'I would like {args[1]} {kwargs['fruit']}')
 
-myfunc(10,20,30,fruit='Apples', food='eggs')
+# myfunc(10,20,30,fruit='Apples', food='eggs')
+
+def add(*args):
+    total = 0
+    for n in args:
+        total += n
+    return total
+
+result = add(1,2,3,4,5,6)
+# print(result)
+
+class Car:
+
+    def __init__(self, **kw):
+        # self.make = kw['make']
+        # self.model = kw['model']
+
+        self.make = kw.get('make')
+        self.model = kw.get('model')
+        self.color = kw.get('color')
+        self.seats = kw.get('seats')
+
+car = Car(make='Nissan', model='GT-R')
+print(car.make, car.model, car.color)
