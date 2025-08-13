f5f73ffc51a3c321fc06967d798e044404de7f7f
Avinash Anand <avinash.rkl09@gmail.com>
2025-08-13 23:06:17 +0530

    Tkinter: practicing few more tkinter widgets

--- Tkinter/Tkinter.py
+++ Tkinter/Tkinter.py
@@ -29,5 +29,45 @@ button.pack()
 input = tkinter.Entry(width=20)
 input.pack()
 
+#Text Box
+textbox = tkinter.Text(width=30, height=5)
+textbox.insert(tkinter.END, "Enter new text here.")
+textbox.focus()
+textbox.pack()
+
+#Spin box
+def spinbox_action():
+    print(spin.get())
+spin = tkinter.Spinbox(from_=0, to=10, width=5, command=spinbox_action)
+spin.pack()
+
+#Checkbox
+def show_state():
+    print(choice.get())
+choice = tkinter.IntVar()
+checkbutton = tkinter.Checkbutton(text="I agree", variable=choice, command=show_state)
+choice.get()
+checkbutton.pack()
+
+#Radio Buttons
+def radio_state():
+    print(radio_choice.get())
+
+radio_choice = tkinter.IntVar()
+radio1 = tkinter.Radiobutton(text="Option 1", variable=radio_choice, value=1, command=radio_state)
+radio2 = tkinter.Radiobutton(text="Option 2", variable=radio_choice, value=2, command=radio_state)
+radio1.pack()
+radio2.pack()
+
+#List Box
+def listbox_used(event):
+    print(listbox.get(listbox.curselection()))
+
+listbox = tkinter.Listbox(height=4)
+fruits = ['Apple','Banana','Peach','Orange']
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
 
 window.mainloop() 
