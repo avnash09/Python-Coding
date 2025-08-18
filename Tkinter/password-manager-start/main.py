ce5ec4b1739208a7ea4261385b7aaa5a96670618
Avinash Anand <avinash.rkl09@gmail.com>
2025-08-18 21:54:26 +0530

    Password Manager: Code v1.0-Working code, App generates & saves passwords for entered websites

--- Tkinter/password-manager-start/main.py
+++ Tkinter/password-manager-start/main.py
@@ -1,9 +1,10 @@
 from tkinter import *
-import os, random
+from tkinter import messagebox
+import os, random, pyperclip
 
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '@']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '%', '+', '@']
 NO_OF_LETTERS = (8, 9, 10, 11, 12)
 
 if os.name == 'nt':os.system('cls')
@@ -13,45 +14,50 @@ else: os.system('clear')
 dir_path = "./Tkinter/password-manager-start/"
 if not os.path.exists(dir_path):
     # os.makedirs(dir_path)
-    print("folder created")
-else:
-    print("folder exists")
+    print("New folder created.")
+# else:
+#     print("folder exists")
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
 def generate_password():
-    pwd = ""
-    while len(pwd) not in NO_OF_LETTERS:
-        letters = random.sample(LETTERS, random.randint(1, 10))
-        nums = random.sample(NUMBERS, random.randint(1, 10))
-        syms = random.sample(SYMBOLS, random.randint(1, 10))
+    password = ""
+    while len(password) not in NO_OF_LETTERS:
+        letters = random.sample(LETTERS, random.randint(6, 10))
+        nums = random.sample(NUMBERS, random.randint(2, 4))
+        syms = random.sample(SYMBOLS, random.randint(2, 4))
         #combine all the characters in a list
-        pwd_list = (letters + nums + syms)
+        password_list = (letters + nums + syms)
         #Shuffle the characters
-        random.shuffle(pwd_list)
+        random.shuffle(password_list)
         #Create a password string out of the shuffled list
-        pwd = ''.join(pwd_list)
-    print(pwd)
-    password_entry.delete(0, END)
-    password_entry.insert(0, string=pwd)
-    print('password generated')
+        password = ''.join(password_list)
+
+    password_entry.delete(0, END)   #clears the password entry pane
+    password_entry.insert(0, string=password)   #shows the password generated in the entry pane
+    #Copy the password to the clipboard automatically
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 def save_entry():
     website_name = website_entry.get().strip()
-    email_id = email_entry.get()
+    email_id = email_entry.get().strip()
     password = password_entry.get().strip()
 
     if not website_name:
         print('Enter Website name.')
-    elif not password_entry:
+        messagebox.showwarning(title="Error", message="Please enter website.")
+    elif not password:
         print('Enter password')
-    if not os.path.exists(dir_path+"data.txt"):
-        write_mode = 'w'
+        messagebox.showwarning(title="Error", message="Please enter a password.")
     else:
-        write_mode = 'a'
-    write_mode = 'w'
-    
-    with open(dir_path+"data.txt", mode=write_mode) as data_file:
-        data_file.write(f"{website_name} | {email_id} | {password}\n")
+        is_ok = messagebox.askokcancel(title=website_name, message=f"There are the details entered:\nEmail: {email_id}\nIs it ok to save?")
+        if is_ok:
+            with open(dir_path+"data.txt", mode='a') as data_file:
+                data_file.write(f"{website_name} | {email_id} | {password}\n")
+            #wipe the entries clean from the window
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -82,7 +88,6 @@ email_entry.insert(END, "avinash@gmail.com")
 email_entry.grid(column=1, row=2, columnspan=2)
 
 password_entry = Entry(width=35, justify='left')
-password_entry.insert(0, 'Enter Password')
 password_entry.grid(column=1, row=3)
 
 gen_pwd_button = Button(text="Generate Password", justify='left', command=generate_password)
