deca457f5cfb6ea07017d0151f32b19198b40277
Avinash Anand <avinash.rkl09@gmail.com>
2025-08-19 19:22:47 +0530

    Password Manager: Code v1.2-Changed file format from txt to json for data store file, Code is updated to read the & write to the json file.

--- Tkinter/password-manager-start/main.py
+++ Tkinter/password-manager-start/main.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
-import os, random, pyperclip
-
+import os, random, pyperclip, pandas as pd, pathlib, json
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '%', '+', '@']
 NO_OF_LETTERS = (8, 9, 10, 11, 12)
+DATA_STORE_HEADERS = ['Website', 'Email', 'Password']
 
 if os.name == 'nt':os.system('cls')
 else: os.system('clear')
@@ -37,10 +37,39 @@ def generate_password():
     #Copy the password to the clipboard automatically
     pyperclip.copy(password)
 
+# ---------------------------- SEARCH WEBSITE ------------------------------- #
+def search():
+
+    input_website = website_entry.get().strip().capitalize()
+    # data_file = dir_path+'data.txt'
+    data_file = dir_path+'data.json'
+    password = ''
+
+    ####################################################################################################################
+    ############################## Intentionally commented this code ###################################################
+    # data = pd.read_csv(data_file, header=None, index_col=False, delimiter='|', names=DATA_STORE_HEADERS)
+    # websites = [site.strip() for site in data.Website.unique()]
+    # if input_website in websites:
+    #     line = data[data.Website == input_website+' ']
+    #     email = line.Email.item()
+    #     password = line.Password.item()
+    #     messagebox.showinfo(title=f"{input_website} details", message=f"Email: {email}\nPassword: {password}\n")
+    ####################################################################################################################
+
+    ####################################################################################################################
+    ############ Code placeholder for update password functionality ####################################################
+    # else:
+    #     pass
+        # upd_pwd = messagebox.askyesno("Update Password", message="Do you want to update your password?")
+        # if upd_pwd:
+        #     data.loc[data.Website == input_website, 'Password'] = password
+        #     data.to_csv(data_file, sep="|", header=False, index=False)
+    ####################################################################################################################
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_entry():
-    website_name = website_entry.get().strip()
+    website_name = website_entry.get().strip().capitalize()
     email_id = email_entry.get().strip()
     password = password_entry.get().strip()
 
@@ -49,16 +78,43 @@ def save_entry():
         messagebox.showwarning(title="Error", message="Please enter website.")
     elif not password:
         print('Enter password')
-        messagebox.showwarning(title="Error", message="Please enter a password.")
+        messagebox.showwarning(title="Error", message="Please enter a password.")            
     else:
-        is_ok = messagebox.askokcancel(title=website_name, message=f"Wesbite: {website_name}\nEmail: {email_id}\nOk to Save?")
-        if is_ok:
-            with open(dir_path+"data.txt", mode='a') as data_file:
-                data_file.write(f"{website_name} | {email_id} | {password}\n")
-            messagebox.showinfo(title="Success", message="Data Store saved successfully.")
-            #wipe the entries clean from the window
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+        ####################################################################################################################
+        ############################## Intentionally commented this code ###################################################
+        # is_ok = messagebox.askokcancel(title=website_name, message=f"Wesbite: {website_name}\nEmail: {email_id}\nOk to Save?")
+        # if is_ok:
+        #     with open(dir_path+"data.txt", mode='a') as data_file:
+        #         data_file.write(f"{website_name.title()} | {email_id} | {password}\n")
+        ####################################################################################################################
+
+        #Changing the file format to json
+        #New data mimicking the json format
+        new_data = {
+            website_name: {
+                'email': email_id,
+                'password': password
+            }
+        }
+
+        try:
+            with open(dir_path+'data.json', mode='r') as data_file:
+                #Reading the data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            data = new_data
+        else:
+            #updating old data with new data
+            data.update(new_data)
+        finally:
+            with open(dir_path+'data.json', mode='w') as data_file:
+                #saving the updated data
+                json.dump(data, data_file, indent=4)
+
+        messagebox.showinfo(title="Success", message="Data Store saved successfully.")
+        #wipe the entries clean from the window
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -80,8 +136,8 @@ email_label.grid(column=0, row=2)
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-website_entry = Entry(width=54, justify='left')
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=35, justify='left')
+website_entry.grid(column=1, row=1, columnspan=1)
 website_entry.focus()
 
 email_entry = Entry(width=54, justify='left')
@@ -96,5 +152,8 @@ gen_pwd_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=46, justify='left', command=save_entry)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", width=15, command=search)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
