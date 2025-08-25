d780098f0ffe380797cc950d27ff005cd24b0b7e
Avinash Macbook Air <avinash.rkl09@gmail.com>
2025-08-25 23:33:19 +0530

    Birthday Manager: Code v0.6-added save_entry(), reset_fields() functions, Combobox fields are now select only, Typing is restricted

--- Tkinter/birthday-manager/birthday_manager_main.py
+++ Tkinter/birthday-manager/birthday_manager_main.py
@@ -11,7 +11,7 @@ MONTHS = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
 ]
-
+MONTHS_DICT = {month : (index+1, month[:3]) for index, month in enumerate(MONTHS)}
 YEARS = [year for year in range(1980, dt.datetime.now().year + 1)]
 
 filename = 'birthday_manager.png'
@@ -29,18 +29,31 @@ label = Label(text="Welcome to Birthday Manager")
 # label.grid(row=0, column=0, columnspan=9)
 
 canvas = Canvas(width=300, height=240)
-bday_mngr_image = PhotoImage(file=f"./SMTP/birthday-wisher-extrahard-start/{filename}")
+bday_mngr_image = PhotoImage(file=f"./Tkinter/birthday-manager/{filename}")
 canvas.create_image(150, 120, image=bday_mngr_image)
 canvas.grid(row=1, column=1, columnspan=6, padx=10)
 
-def save_entry():
-    print(f"Birthday: {birthday_date}-{birthday_month}-{birthday_year}")
+def save_entry(event=None):
+    
+    if birthday_date == '' or birthday_month == '' or birthday_year == '':
+        messagebox.showerror(title="Error", message="Invalid date")
+    else:
+        person = name_entry.get().strip().title()
+        print(person)
+        email = email_entry.get().strip()
+        print(email)
+        print(f"Birthday: {birthday_date}-{MONTHS_DICT[birthday_month][1]}-{birthday_year}")
+
+        reset_fields()
+
+def reset_fields():
+    name_entry.delete(0, END)
+    year_dropdown.set('year')
+    month_dropdown.set('month')
+    date_dropdown.set('date')
+    email_entry.delete(0, END)
 
 def get_name(event=None):
-    person = name_entry.get().strip().title()
-    print(person)
-    if birthday_date == '' or birthday_month == '' or birthday_year == '':
-        messagebox.showerror()
     save_entry()
 
 def get_date(event):
@@ -53,14 +66,14 @@ def get_month(event):
     global birthday_month
     print('inside get_month: ',month_dropdown.get()) 
     birthday_month = month_dropdown.get()
-    is_valid_date()
+    get_valid_date()
 
 def get_year(event):
     global birthday_year
     print('inside get_year:', year_dropdown.get())
     birthday_year = year_dropdown.get()
     print(f"Birthday year {birthday_year}")
-    is_valid_date()
+    get_valid_date()
 
 def is_leap_year(year_input) -> bool:
     try:
@@ -76,7 +89,7 @@ def is_leap_year(year_input) -> bool:
     except:
         print('Unknown error.')
     
-def is_valid_date(year='', month='', date=''):
+def get_valid_date(year='', month='', date=''):
     date = birthday_date
     month = birthday_month
     year = birthday_year
@@ -116,7 +129,7 @@ name_entry.grid(row=2, column=2, columnspan=6, pady=5, sticky='w')
 birthday_label = Label(text='Birthday: ', width=10, anchor='e')
 birthday_label.grid(row=3, column=1, pady=5)
 
-date_dropdown = ttk.Combobox(values='', width=7)
+date_dropdown = ttk.Combobox(values='', width=7, state='readonly')
 date_dropdown.set("date")
 date_dropdown.grid(row=3, column=4)
 date_dropdown.bind("<<ComboboxSelected>>", get_date)
@@ -124,7 +137,7 @@ date_dropdown.bind("<<ComboboxSelected>>", get_date)
 delimiter_label1 = Label(text="/", width=1)
 # delimiter_label1.grid(row=3, column=3, sticky='ew')
     
-month_dropdown = ttk.Combobox(values=MONTHS, width=7)
+month_dropdown = ttk.Combobox(values=MONTHS, width=7, state='readonly')
 month_dropdown.set("month")
 month_dropdown.grid(row=3, column=3)
 month_dropdown.bind("<<ComboboxSelected>>", get_month)
@@ -132,14 +145,20 @@ month_dropdown.bind("<<ComboboxSelected>>", get_month)
 delimiter_label2 = Label(text="/", width=1)
 # delimiter_label2.grid(row=3, column=5, sticky='ew')
 
-year_dropdown = ttk.Combobox(values=YEARS, width=7)
+year_dropdown = ttk.Combobox(values=YEARS, width=7, state='readonly')
 year_dropdown.set("year")
 year_dropdown.grid(row=3, column=2)
 year_dropdown.bind("<<ComboboxSelected>>", get_year)
 
-save_button = Button(text="Save", width=9, command=get_name)
-save_button.grid(row=4, column=2, columnspan=3, pady=5)
-window.bind("<Return>", lambda event: get_name())
+email_label = Label(text="Email ID: ",width=10, anchor='e')
+email_label.grid(row=4, column=1)
+
+email_entry = Entry(width=35)
+email_entry.grid(row=4, column=2, columnspan=6, pady=5)
+
+save_button = Button(text="Save", width=9, command=save_entry)
+save_button.grid(row=5, column=2, columnspan=3, pady=5)
+window.bind("<Return>", lambda event: save_entry())
 
 
 window.mainloop()
